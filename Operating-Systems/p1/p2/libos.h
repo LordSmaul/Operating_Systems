b50f7a573e9889851373406895a07b8bdf6aaa51
Samuel Schmahl <105804551+LordSmaul@users.noreply.github.com>
2024-11-23 13:28:59 -0600

    Delete Operating-Systems/p1/p2 directory

--- Operating-Systems/p1/p2/libos.h
+++
@@ -1,4 +0,0 @@
-int term_txtheight();
-int term_txtwidth();
-void print_to(unsigned int row, unsigned int col, const char *str);
-void putc_to(unsigned int row, unsigned int col, const char c);
--- Operating-Systems/p1/p2/process.h
+++
@@ -1,9 +0,0 @@
-#ifndef PROCESS_H
-#define PROCESS_H
-#include "queue.h"
-
-uint64_t* alloc_stack();
-PCB_t* alloc_pcb();
-int create_process(int (*code_address)());
-
-#endif
